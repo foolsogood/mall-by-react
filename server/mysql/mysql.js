7b5b281bf5604496946a940204db1c0866d66e0f
foolsogood <851809776@qq.com>
2018-07-01 00:18:58 +0800

    change let into const

--- server/mysql/mysql.js
+++ server/mysql/mysql.js
@@ -1,12 +1,12 @@
-let mysql = require('mysql');
+const mysql = require('mysql');
 const config = require('../config/config')
-let pool = mysql.createPool({
+const pool = mysql.createPool({
     host: config.database.HOST,
     user: config.database.USERNAME,
     password: config.database.PASSWORD,
     database: config.database.DATABASE
 })
-let query = (sql, values) => {
+const query = (sql, values) => {
     return new Promise((resolve, reject) => {
         pool.getConnection((err, connection) => {
             if (err) {
@@ -24,25 +24,25 @@ let query = (sql, values) => {
         })
     })
 }
-let createTable = (sql) => {
+const createTable = (sql) => {
     return query(sql, [])
 }
 
 
-let addCategory = (value) => {
-    let _sql = `INSERT INTO category(cate,cateId) values(?,?);`
+const addCategory = (value) => {
+    const _sql = `INSERT INTO category(cate,cateId) values(?,?);`
     return query(_sql, value)
 }
-let addGood = (value) => {
-    let _sql = `INSERT INTO goods(cate,cateId,goodId,goodName,desction,imgs,detailImg,price) values(?,?,?,?,?,?,?,?);`
+const addGood = (value) => {
+    const _sql = `INSERT INTO goods(cate,cateId,goodId,goodName,desction,imgs,detailImg,price) values(?,?,?,?,?,?,?,?);`
     return query(_sql, value)
 }
-let addBanner = (value) => {
-    let _sql = `INSERT INTO banner(imgId,url) values(?,?);`
+const addBanner = (value) => {
+    const _sql = `INSERT INTO banner(imgId,url) values(?,?);`
     return query(_sql, value)
 }
-let addComment = (value) => {
-    let _sql = `INSERT INTO comments(goodId,avatar,name,rateScore,comment,time) values(?,?,?,?,?,?);`
+const addComment = (value) => {
+    const _sql = `INSERT INTO comments(goodId,avatar,name,rateScore,comment,time) values(?,?,?,?,?,?);`
     return query(_sql, value)
 }
 module.exports = {
