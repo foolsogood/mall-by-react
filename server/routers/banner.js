8a36aeb668a9c5e822c24cfa4bed3f899c41a303
foolsogood <851809776@qq.com>
2018-10-15 20:55:04 +0800

    ETag由sha1加密改etag

--- server/routers/banner.js
+++ server/routers/banner.js
@@ -1,18 +1,18 @@
 const router = require('koa-router')();
 const model = require('../mysql/mysql')
-const sha1 = require('sha1')
-
+const etag=require('etag');
 const { success, fail, tokenInvalid } = require('../config/config').codeOption
 
 router.prefix('/api/banner')
 router.get('/getHomeBanner', async (ctx) => {
     await model.getHomeBanner().then(res => {
         const ifNoneMatch = ctx.get('if-none-match')
+        const _ETAG=etag(JSON.stringify(res))
         ctx.set({
             'Cache-Control': 'max-age=30,no-cache',
-            'ETag': sha1(res)
+            'ETag': _ETAG
         })
-        if (ifNoneMatch == sha1(res)) {
+        if (ifNoneMatch == _ETAG) {
             ctx.status = 304
             ctx.body = ''
         } else {
